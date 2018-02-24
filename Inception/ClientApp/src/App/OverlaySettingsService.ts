cc3bcb9f9261aadf69f054c7637e82457b6c0a0e
VikkyMaru <sunnybunnydev@gmail.com>
2018-02-24 22:06:07 +0200

    Code adj.

--- Inception/ClientApp/src/App/OverlaySettingsService.ts
+++ Inception/ClientApp/src/App/OverlaySettingsService.ts
@@ -4,14 +4,14 @@ import { BehaviorSubject } from "rxjs/BehaviorSubject";
 @Injectable()
 export class OverlaySettingsService 
 {
-    private _isOverlayShown = new BehaviorSubject<boolean>(false);
+    public _isOverlayShown = new BehaviorSubject<boolean>(false);
     isOverlayShown = this._isOverlayShown.asObservable();
     
     constructor()
     {
     }
 
-    changeOverlaySetting(setting: boolean) 
+    public changeOverlaySetting(setting: boolean) 
     {
         this._isOverlayShown.next(setting)
     }
