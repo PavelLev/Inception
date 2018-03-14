0617b3bc4e9abfa57a92bdda7d6a74009e7979fe
VikkyMaru <sunnybunnydev@gmail.com>
2018-03-14 14:21:09 +0200

    Implemented RouteReuseStrategy to be able to detach for specific route.

--- Inception/ClientApp/src/App/Home/InceptionRouteReuseStrategy.ts
+++ Inception/ClientApp/src/App/Home/InceptionRouteReuseStrategy.ts
@@ -1,28 +1,28 @@
 import { RouteReuseStrategy, DetachedRouteHandle, ActivatedRouteSnapshot } from "@angular/router";
 
 export class InceptionRouteReuseStrategy implements RouteReuseStrategy {
-
-    handlers: {[key: string]: DetachedRouteHandle} = {};
+    private _handlers: {[key: string]: DetachedRouteHandle} = {};
+    private _acceptedRoutes: string[] = [""];
 
     shouldDetach(route: ActivatedRouteSnapshot): boolean {
-        console.debug('CustomReuseStrategy:shouldDetach', route);
-        return true;
+        let result = this._acceptedRoutes.includes(route.routeConfig.path);
+        return result;
     }
 
     store(route: ActivatedRouteSnapshot, handle: DetachedRouteHandle): void {
         console.debug('CustomReuseStrategy:store', route, handle);
-        this.handlers[route.routeConfig.path] = handle;
+        this._handlers[route.routeConfig.path] = handle;
     }
 
     shouldAttach(route: ActivatedRouteSnapshot): boolean {
         console.debug('CustomReuseStrategy:shouldAttach', route);
-        return !!route.routeConfig && !!this.handlers[route.routeConfig.path];
+        return !!route.routeConfig && !!this._handlers[route.routeConfig.path];
     }
 
     retrieve(route: ActivatedRouteSnapshot): DetachedRouteHandle {
         console.debug('CustomReuseStrategy:retrieve', route);
         if (!route.routeConfig) return null;
-        return this.handlers[route.routeConfig.path];
+        return this._handlers[route.routeConfig.path];
     }
 
     shouldReuseRoute(future: ActivatedRouteSnapshot, curr: ActivatedRouteSnapshot): boolean {
