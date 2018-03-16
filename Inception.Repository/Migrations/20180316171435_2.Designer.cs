9f2cbbe5cdcd193c9e48ea6f68c08e4698b56606
VikkyMaru <sunnybunnydev@gmail.com>
2018-03-16 19:19:45 +0200

    Created a new migration with DomainName column in SiteTestOverview table.

--- Inception.Repository/Migrations/20180316171435_2.Designer.cs
+++ Inception.Repository/Migrations/20180316171435_2.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace Inception.Repository.Migrations
 {
     [DbContext(typeof(InceptionDbContext))]
-    partial class InceptionDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180316171435_2")]
+    partial class _2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Inception.Repository/Migrations/20180316171435_2.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Inception.Repository.Migrations
+{
+    public partial class _2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DomainName",
+                table: "SiteTestOverviews",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DomainName",
+                table: "SiteTestOverviews");
+        }
+    }
+}
